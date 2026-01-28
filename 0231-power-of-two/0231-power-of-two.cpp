e087ed9e3acdffcec58416c3b77733ea51495d71
Abhishek kumar <abhishekji763190@gmail.com>
2026-01-28 19:20:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if((n & (n-1))==0)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+};
