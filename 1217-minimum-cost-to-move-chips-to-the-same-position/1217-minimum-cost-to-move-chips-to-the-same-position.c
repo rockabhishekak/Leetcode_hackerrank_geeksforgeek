bb440f57ef6227616a65793c835a21817a86638e
Abhishek kumar <abhishekji763190@gmail.com>
2026-02-02 16:43:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (19.23%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.c
@@ -0,0 +1,14 @@
+int minCostToMoveChips(int* position, int positionSize) {
+    int even = 0;
+    int odd = 0;
+
+    for (int i = 0; i < positionSize; i++) {
+        if (position[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    return even < odd ? even : odd;
+}
