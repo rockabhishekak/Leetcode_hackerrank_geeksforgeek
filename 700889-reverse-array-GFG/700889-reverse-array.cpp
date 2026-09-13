56ac8267c6e71be46ba8a5a819466f16d0f22d44
Abhishek kumar <abhishekji763190@gmail.com>
2026-09-13 17:00:47 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 700889-reverse-array-GFG/700889-reverse-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int l = 0;
+        int r = arr.size() - 1;
+
+        while (l < r) {
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
